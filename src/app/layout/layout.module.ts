e55c8850a726d8f807663b1f02e8b666388f5033
wii <i23253@firstbank.com.tw>
2023-12-07 15:13:36 +0800

    init

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { LayoutComponent } from 'app/layout/layout.component';
+import { EmptyLayoutModule } from 'app/layout/layouts/empty/empty.module';
+import { ModernLayoutModule } from 'app/layout/layouts/horizontal/modern/modern.module';
+import { SettingsModule } from 'app/layout/common/settings/settings.module';
+import { SharedModule } from 'app/shared/shared.module';
+
+const layoutModules = [
+    // Empty
+    EmptyLayoutModule,
+    ModernLayoutModule,
+];
+
+@NgModule({
+    declarations: [
+        LayoutComponent
+    ],
+    imports     : [
+        SharedModule,
+        SettingsModule,
+        ...layoutModules
+    ],
+    exports     : [
+        LayoutComponent,
+        ...layoutModules
+    ]
+})
+export class LayoutModule
+{
+}
