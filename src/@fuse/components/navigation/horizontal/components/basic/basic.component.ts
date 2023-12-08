5172bd3d8a0b1fc6e54bb0a09dc38d8b79597ab2
wii <i23253@firstbank.com.tw>
2023-12-08 16:47:52 +0800

    style: home page

--- src/@fuse/components/navigation/horizontal/components/basic/basic.component.ts
+++ src/@fuse/components/navigation/horizontal/components/basic/basic.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { IsActiveMatchOptions } from '@angular/router';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, Subscription, takeUntil } from 'rxjs';
 import { FuseHorizontalNavigationComponent } from '@fuse/components/navigation/horizontal/horizontal.component';
 import { FuseNavigationService } from '@fuse/components/navigation/navigation.service';
 import { FuseNavigationItem } from '@fuse/components/navigation/navigation.types';
 import { FuseUtilsService } from '@fuse/services/utils/utils.service';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector       : 'fuse-horizontal-navigation-basic-item',
@@ -16,17 +18,19 @@ export class FuseHorizontalNavigationBasicItemComponent implements OnInit, OnDes
     @Input() item: FuseNavigationItem;
     @Input() name: string;
 
+
     isActiveMatchOptions: IsActiveMatchOptions;
     private _fuseHorizontalNavigationComponent: FuseHorizontalNavigationComponent;
     private _unsubscribeAll: Subject<any> = new Subject<any>();
-
+    private iconSubscription: Subscription;
     /**
      * Constructor
      */
     constructor(
         private _changeDetectorRef: ChangeDetectorRef,
         private _fuseNavigationService: FuseNavigationService,
-        private _fuseUtilsService: FuseUtilsService
+        private _fuseUtilsService: FuseUtilsService,
+        private _matIconRegistry: MatIconRegistry, private _domSanitizer: DomSanitizer
     )
     {
         // Set the equivalent of {exact: false} as default for active match options.
@@ -34,6 +38,7 @@ export class FuseHorizontalNavigationBasicItemComponent implements OnInit, OnDes
         // [routerLinkActiveOptions] because if it's "undefined" initially, the router
         // will throw an error and stop working.
         this.isActiveMatchOptions = this._fuseUtilsService.subsetMatchOptions;
+
     }
 
     // -----------------------------------------------------------------------------------------------------
@@ -67,6 +72,7 @@ export class FuseHorizontalNavigationBasicItemComponent implements OnInit, OnDes
             // Mark for check
             this._changeDetectorRef.markForCheck();
         });
+        this.registerCustomIcons();
     }
 
     /**
@@ -77,5 +83,53 @@ export class FuseHorizontalNavigationBasicItemComponent implements OnInit, OnDes
         // Unsubscribe from all subscriptions
         this._unsubscribeAll.next(null);
         this._unsubscribeAll.complete();
+        this.iconSubscription.unsubscribe();
     }
+
+    // aa(item?: any): void{
+    //     switch (item.id){
+    //         case 'home':
+    //             item.icon='assets/icons/tab1-red.svg';
+    //         break;
+    //         case 'folder':
+    //             item.icon='assets/icons/tab2-red.svg';
+    //         break;
+    //         case 'odm':
+    //             item.icon='assets/icons/tab3-red.svg';
+    //         break;
+    //         case 'custom':
+    //             item.icon='assets/icons/tab4-red.svg';
+    //         break;
+    //         case 'account':
+    //             item.icon='assets/icons/tab5-red.svg';
+    //         break;
+    //         default:
+    //         break;
+    //     }
+
+    // }
+
+    registerCustomIcons(): void {
+        // Register your custom SVG icon
+        this._matIconRegistry.addSvgIcon(
+          'tab1',
+          this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/tab1.svg')
+        );
+        this._matIconRegistry.addSvgIcon(
+            'tab2',
+            this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/tab2.svg')
+          );
+          this._matIconRegistry.addSvgIcon(
+            'tab3',
+            this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/tab3.svg')
+          );
+          this._matIconRegistry.addSvgIcon(
+            'tab4',
+            this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/tab4.svg')
+          );
+          this._matIconRegistry.addSvgIcon(
+            'tab5',
+            this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/tab5.svg')
+          );
+      }
 }
--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -2,134 +2,134 @@
 import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
-    {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
-    },{
-        id   : 'home',
-        title: '首頁',
-        type : 'basic',
-        icon : 'assets/icons/tab1.svg',
-        link : '/home'
-    },
-    {
-        id   : 'folder',
-        title: '需求草稿夾',
-        type : 'basic',
-        icon : 'assets/icons/tab2.svg',
-        link : '/folder'
-    },
-    {
-        id   : 'odm',
-        title: 'ODM草稿夾',
-        type : 'basic',
-        icon : 'assets/icons/tab3.svg',
-        link : '/odm'
-    },
-    {
-        id   : 'custom',
-        title: '客戶',
-        type : 'basic',
-        icon : 'assets/icons/tab4.svg',
-        link : '/custom'
-    },
-    {
-        id   : 'account',
-        title: '帳號',
-        type : 'basic',
-        icon : 'assets/icons/tab5.svg',
-        link : '/account'
-    }
+    // {
+    //     id   : 'example',
+    //     title: 'Example',
+    //     type : 'basic',
+    //     icon : 'heroicons_outline:chart-pie',
+    //     link : '/example'
+    // },{
+    //     id   : 'home',
+    //     title: '首頁',
+    //     type : 'basic',
+    //     icon : 'heroicons_outline:home',
+    //     link : '/home'
+    // },
+    // {
+    //     id   : 'folder',
+    //     title: '需求草稿夾',
+    //     type : 'basic',
+    //     icon : 'heroicons_outline:home',
+    //     link : '/folder'
+    // },
+    // {
+    //     id   : 'odm',
+    //     title: 'ODM草稿夾',
+    //     type : 'basic',
+    //     icon : 'heroicons_outline:home',
+    //     link : '/odm'
+    // },
+    // {
+    //     id   : 'custom',
+    //     title: '客戶',
+    //     type : 'basic',
+    //     icon : 'heroicons_outline:home',
+    //     link : '/custom'
+    // },
+    // {
+    //     id   : 'account',
+    //     title: '帳號',
+    //     type : 'basic',
+    //     icon : 'heroicons_outline:home',
+    //     link : '/account'
+    // }
 ];
 export const compactNavigation: FuseNavigationItem[] = [
-    {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
-    },{
-        id   : 'home',
-        title: '首頁',
-        type : 'basic',
-        icon : 'assets/icons/tab1.svg',
-        link : '/home'
-    },
-    {
-        id   : 'folder',
-        title: '需求草稿夾',
-        type : 'basic',
-        icon : 'assets/icons/tab2.svg',
-        link : '/folder'
-    },
-    {
-        id   : 'odm',
-        title: 'ODM草稿夾',
-        type : 'basic',
-        icon : 'assets/icons/tab3.svg',
-        link : '/odm'
-    },
-    {
-        id   : 'custom',
-        title: '客戶',
-        type : 'basic',
-        icon : 'assets/icons/tab4.svg',
-        link : '/custom'
-    },
-    {
-        id   : 'account',
-        title: '帳號',
-        type : 'basic',
-        icon : 'assets/icons/tab5.svg',
-        link : '/account'
-    }
+    // {
+    //     id   : 'example',
+    //     title: 'Example',
+    //     type : 'basic',
+    //     icon : 'heroicons_outline:chart-pie',
+    //     link : '/example'
+    // },{
+    //     id   : 'home',
+    //     title: '首頁',
+    //     type : 'basic',
+    //     icon : 'assets/icons/tab1.svg',
+    //     link : '/home'
+    // },
+    // {
+    //     id   : 'folder',
+    //     title: '需求草稿夾',
+    //     type : 'basic',
+    //     icon : 'assets/icons/tab2.svg',
+    //     link : '/folder'
+    // },
+    // {
+    //     id   : 'odm',
+    //     title: 'ODM草稿夾',
+    //     type : 'basic',
+    //     icon : 'assets/icons/tab3.svg',
+    //     link : '/odm'
+    // },
+    // {
+    //     id   : 'custom',
+    //     title: '客戶',
+    //     type : 'basic',
+    //     icon : 'assets/icons/tab4.svg',
+    //     link : '/custom'
+    // },
+    // {
+    //     id   : 'account',
+    //     title: '帳號',
+    //     type : 'basic',
+    //     icon : 'assets/icons/tab5.svg',
+    //     link : '/account'
+    // }
 ];
 export const futuristicNavigation: FuseNavigationItem[] = [
-    {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
-    },
-    {
-        id   : 'home',
-        title: '首頁',
-        type : 'basic',
-        icon : 'assets/icons/tab1.svg',
-        link : '/home'
-    },
-    {
-        id   : 'folder',
-        title: '需求草稿夾',
-        type : 'basic',
-        icon : 'assets/icons/tab2.svg',
-        link : '/folder'
-    },
-    {
-        id   : 'odm',
-        title: 'ODM草稿夾',
-        type : 'basic',
-        icon : 'assets/icons/tab3.svg',
-        link : '/odm'
-    },
-    {
-        id   : 'custom',
-        title: '客戶',
-        type : 'basic',
-        icon : 'assets/icons/tab4.svg',
-        link : '/custom'
-    },
-    {
-        id   : 'account',
-        title: '帳號',
-        type : 'basic',
-        icon : 'assets/icons/tab5.svg',
-        link : '/account'
-    }
+    // {
+    //     id   : 'example',
+    //     title: 'Example',
+    //     type : 'basic',
+    //     icon : 'heroicons_outline:chart-pie',
+    //     link : '/example'
+    // },
+    // {
+    //     id   : 'home',
+    //     title: '首頁',
+    //     type : 'basic',
+    //     icon : 'assets/icons/tab1.svg',
+    //     link : '/home'
+    // },
+    // {
+    //     id   : 'folder',
+    //     title: '需求草稿夾',
+    //     type : 'basic',
+    //     icon : 'assets/icons/tab2.svg',
+    //     link : '/folder'
+    // },
+    // {
+    //     id   : 'odm',
+    //     title: 'ODM草稿夾',
+    //     type : 'basic',
+    //     icon : 'assets/icons/tab3.svg',
+    //     link : '/odm'
+    // },
+    // {
+    //     id   : 'custom',
+    //     title: '客戶',
+    //     type : 'basic',
+    //     icon : 'assets/icons/tab4.svg',
+    //     link : '/custom'
+    // },
+    // {
+    //     id   : 'account',
+    //     title: '帳號',
+    //     type : 'basic',
+    //     icon : 'assets/icons/tab5.svg',
+    //     link : '/account'
+    // }
 ];
 export const horizontalNavigation: FuseNavigationItem[] = [
     // {
@@ -139,39 +139,39 @@ export const horizontalNavigation: FuseNavigationItem[] = [
     //     icon : 'heroicons_outline:chart-pie',
     //     link : '/example'
     // },
-{
-    id   : 'home',
-    title: '首頁',
-    type : 'basic',
-    icon : 'assets/icons/tab1.svg',
-    link : '/home'
-},
-{
-    id   : 'folder',
-    title: '需求草稿夾',
-    type : 'basic',
-    icon : 'assets/icons/tab2.svg',
-    link : '/folder'
-},
-{
-    id   : 'odm',
-    title: 'ODM草稿夾',
-    type : 'basic',
-    icon : 'assets/icons/tab3.svg',
-    link : '/odm'
-},
-{
-    id   : 'custom',
-    title: '客戶',
-    type : 'basic',
-    icon : 'assets/icons/tab4.svg',
-    link : '/custom'
-},
-{
-    id   : 'account',
-    title: '帳號',
-    type : 'basic',
-    icon : 'assets/icons/tab5.svg',
-    link : '/account'
-}
+    {
+        id   : 'home',
+        title: '首頁',
+        type : 'basic',
+        icon : 'heroicons_outline:home',
+        link : '/home'
+    },
+    {
+        id   : 'folder',
+        title: '需求草稿夾',
+        type : 'basic',
+        icon : 'heroicons_outline:folder-open',
+        link : '/folder'
+    },
+    {
+        id   : 'odm',
+        title: 'ODM草稿夾',
+        type : 'basic',
+        icon : 'mat_outline:note_alt',
+        link : '/odm'
+    },
+    {
+        id   : 'custom',
+        title: '客戶',
+        type : 'basic',
+        icon : 'mat_outline:people',
+        link : '/custom'
+    },
+    {
+        id   : 'account',
+        title: '帳號',
+        type : 'basic',
+        icon : 'mat_outline:account_circle',
+        link : '/account'
+    }
 ];
