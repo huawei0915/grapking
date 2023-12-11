5236ac8bcfd7fffcd79b176090be9938a58c3f1d
wii <i23253@firstbank.com.tw>
2023-12-11 16:19:18 +0800

    style:recommend page

--- src/app/modules/admin/home/recommend/recommend.component.ts
+++ src/app/modules/admin/home/recommend/recommend.component.ts
@@ -3,6 +3,7 @@ import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 @Component({
     selector     : 'recommend',
     templateUrl  : './recommend.component.html',
+    styleUrls: ['./recommend.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
 export class RecommendComponent implements OnInit
