6064794e74b406fbeaa7b1aa56a20f6a7bf1ecbb
wii <i23253@firstbank.com.tw>
2023-12-16 23:18:06 +0800

    feat: question push data

---
+++ src/app/modules/admin/home/question/mock.ts
@@ -0,0 +1,152 @@
+export const productLevel1 = [{
+    active: false,
+    name: '美容1',
+}, {
+    active: false,
+    name: '腸胃道保健1',
+}, {
+    active: false,
+    name: '排便',
+}, {
+    active: false,
+    name: '免疫調節',
+}, {
+    active: false,
+    name: '腸胃道保健',
+}];
+
+export const productLevel2 = [{
+    active: false,
+    name: '美容2',
+}, {
+    active: false,
+    name: '腸胃道保健2',
+}, {
+    active: false,
+    name: '排便',
+}, {
+    active: false,
+    name: '免疫調節',
+}, {
+    active: false,
+    name: '腸胃道保健',
+}];
+
+export const productLevel3 = [{
+    active: false,
+    name: '美容3',
+}, {
+    active: false,
+    name: '腸胃道保健3',
+}, {
+    active: false,
+    name: '排便',
+}, {
+    active: false,
+    name: '免疫調節',
+}, {
+    active: false,
+    name: '腸胃道保健',
+}];
+
+export const productLevel4 = [{
+    active: false,
+    name: '美容4',
+}, {
+    active: false,
+    name: '腸胃道保健4',
+}, {
+    active: false,
+    name: '排便',
+}, {
+    active: false,
+    name: '免疫調節',
+}, {
+    active: false,
+    name: '腸胃道保健',
+}];
+
+export const productLevel5 = [{
+    active: false,
+    name: '美容5',
+}, {
+    active: false,
+    name: '腸胃道保健5',
+}, {
+    active: false,
+    name: '排便',
+}, {
+    active: false,
+    name: '免疫調節',
+}, {
+    active: false,
+    name: '腸胃道保健',
+}];
+
+export const productLevel6 = [{
+    active: false,
+    name: '美容6',
+}, {
+    active: false,
+    name: '腸胃道保健6',
+}, {
+    active: false,
+    name: '排便',
+}, {
+    active: false,
+    name: '免疫調節',
+}, {
+    active: false,
+    name: '腸胃道保健',
+}];
+
+export const productLevel7 = [{
+    active: false,
+    name: '美容',
+}, {
+    active: false,
+    name: '腸胃道保健',
+}, {
+    active: false,
+    name: '排便',
+}, {
+    active: false,
+    name: '免疫調節',
+}, {
+    active: false,
+    name: '腸胃道保健',
+}];
+
+export const productLevel8 = [{
+    active: false,
+    name: '美容',
+}, {
+    active: false,
+    name: '腸胃道保健',
+}, {
+    active: false,
+    name: '排便',
+}, {
+    active: false,
+    name: '免疫調節',
+}, {
+    active: false,
+    name: '腸胃道保健',
+}];
+
+export const productLevel9 = [{
+    active: false,
+    name: '美容',
+}, {
+    active: false,
+    name: '腸胃道保健',
+}, {
+    active: false,
+    name: '排便',
+}, {
+    active: false,
+    name: '免疫調節',
+}, {
+    active: false,
+    name: '腸胃道保健',
+}];
--- src/app/modules/admin/home/question/question.component.ts
+++ src/app/modules/admin/home/question/question.component.ts
@@ -1,12 +1,39 @@
+import { activities } from './../../../../mock-api/pages/activities/data';
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { ApiService } from '../../api.service';
-
+import { productLevel1, productLevel2, productLevel3, productLevel4, productLevel5, productLevel6, productLevel7, productLevel8, productLevel9 } from './mock';
 @Component({
     selector: 'question',
     templateUrl: './question.component.html',
     encapsulation: ViewEncapsulation.None
 })
 export class QuestionComponent implements OnInit {
+
+    leftList = ['功效', '器官', '年齡', '族群', '國家', '劑型', '包裝', '認證', '葷素'];
+
+    level1 = productLevel1;
+    level2 = productLevel2;
+    level3 = productLevel3;
+    level4 = productLevel4;
+    level5 = productLevel5;
+    level6 = productLevel6;
+    level7 = productLevel7;
+    level8 = productLevel8;
+    level9 = productLevel9;
+    currentLevel = 1; // 初始化當前等級為1
+
+    puoductArr1 = [];
+    puoductArr2 = [];
+    puoductArr3 = [];
+    puoductArr4 = [];
+    puoductArr5 = [];
+    puoductArr6 = [];
+    puoductArr7 = [];
+    puoductArr8 = [];
+    puoductArr9 = [];
+
+    nowLevel = this.level1;
+
     /**
      * Constructor
      */
@@ -20,5 +47,19 @@ export class QuestionComponent implements OnInit {
             console.log(result);
         }).catch((err) => {
         });
+    }
+
+    selectProduct(item, idx: number): void {
+        item.active = !item.active;
+        if (item.active) {
+            this['puoductArr' + this.currentLevel].push(item);
+        }
+    }
+
+    nextStep(): void {
+        if (this.currentLevel < 9) {
+            this.currentLevel += 1;
+            this.nowLevel = this['level' + this.currentLevel];
+        }
     }
 }
