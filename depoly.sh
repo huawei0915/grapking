53e156ba8d07363a6d0ff25a2a23a7b370db3ede
wii <i23253@firstbank.com.tw>
2023-12-18 15:18:44 +0800

    refactor: docker path

--- depoly.sh
+++ depoly.sh
@@ -1,7 +1,7 @@
 # projectName: 專案名稱
 projectName="grap-king"
 # version: 版本
-version="least"
+version="0.0.6"
 # dockerHub: dockerHub帳號
 dockerHub="pksos30066"
 # user: dockerHub帳號
@@ -10,11 +10,22 @@ user="pksos30066@gmail.com"
 pwd="z0929295782z"
 
 # 打包
-docker build -t $projectName:$version .
+docker build -t $projectName:$version -f Dockerfile .
 # 定義tag
 docker tag $projectName:$version $dockerHub/$projectName:$version
 # 登入
 docker login -u $user -p $pwd
 # 上傳
 docker push $dockerHub/$projectName:$version
+
+
+
+# # 打包
+# docker build -t grap-king:least .
+# # 定義tag
+# docker tag grap-king:least $dockerHub/grap-king:least
+# # 登入
+# docker login -u $user -p $pwd
+# # 上傳
+# docker push $dockerHub/grap-king:least
 
