eec678996f2c84b37faf0aa9c15cff38b281cdf3
huangpokai30827 <pokai3309@gmail.com>
2024-01-04 14:07:51 +0800

    add: custom page

--- src/app/modules/admin/custom/custom.module.ts
+++ src/app/modules/admin/custom/custom.module.ts
@@ -1,6 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { Route, RouterModule } from '@angular/router';
+import { TranslocoModule } from '@ngneat/transloco';
 import { CustomComponent } from 'app/modules/admin/custom/custom.component';
+import { SharedModule } from 'app/shared/shared.module';
+import { NgxHmCarouselModule } from 'ngx-hm-carousel';
 
 const customRoutes: Route[] = [
     {
@@ -14,7 +27,22 @@ const customRoutes: Route[] = [
         CustomComponent
     ],
     imports     : [
-        RouterModule.forChild(customRoutes)
+        RouterModule.forChild(customRoutes),
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        FormsModule,
+        NgxHmCarouselModule,
+        TranslocoModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatSlideToggleModule,
+        MatTooltipModule,
+        MatSelectModule,
+        MatCheckboxModule,
+        SharedModule
     ]
 })
 export class CustomModule
