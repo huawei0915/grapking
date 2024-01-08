4b5c5d4dc37dafa17a252d9fa7c836973de08588
huangpokai30827 <pokai3309@gmail.com>
2024-01-08 13:36:42 +0800

    style: home page restyle

--- src/app/modules/admin/home/home.component.ts
+++ src/app/modules/admin/home/home.component.ts
@@ -7,6 +7,12 @@ import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
     encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
+
+    searchAreaCheck = false;
+    questionAreaCheck = false;
+    recommandAreaCheck = false;
+    demmandAreaCheck = false;
+
     /**
      * Constructor
      */
@@ -26,6 +32,19 @@ export class HomeComponent implements OnInit {
         } else {
             this._translocoService.setActiveLang('zh');
         }
+
+    }
+
+    moveToSearchArea(): void {
+
+    }
+    moveToQuestionnaireArea(): void {
+
+    }
+    moveToRecommandArea(): void {
+
+    }
+    moveToDemandArea(): void {
 
     }
 }
